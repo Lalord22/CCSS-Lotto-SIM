f2bd1686908d0b4979b636110c8022b0978745a6
Lalord22 <lalo22296@hotmail.com>
2023-06-10 19:37:50 -0600

    Juego base

--- src/Lotto.java
+++ src/Lotto.java
@@ -90,12 +90,34 @@ private static String arrayToString(int[] array) {
     return sb.toString();
 }
 
-private static void generateRandomArrays(int totalArraysToGenerate ) {
+
+private static int compareArrays(int[] array1, int[] array2) {
+    int count = 0;
+    
+    for (int num1 : array1) {
+        for (int num2 : array2) {
+            if (num1 == num2) {
+                count++;
+                break;
+            }
+        }
+    }
+    
+    return count;
+}
+
+private static void jugarNumeroVeces(int totalArraysToGenerate ) {
 	
 	
+	int costo = 0;
+	int premio = 0;
+	int jackpot = 110300000;
 	
 	int n = 41; // Total number of values
     int r = 5; // Number of values to be generated
+    int[] winner = generateRandomArray(n,r);
+    
+    String arrayWinner = arrayToString(winner);
      
     
     Set<String> generatedArrays = new HashSet<>();
@@ -111,7 +133,31 @@ private static void generateRandomArrays(int totalArraysToGenerate ) {
             System.out.println("Duplicate array generated. Regenerating...");
             i--; // Decrement i to regenerate a new array in the next iteration
         } else {
-            generatedArrays.add(arrayString);
+        	
+        	 costo+=600;
+           
+        	 generatedArrays.add(arrayString);
+            
+           
+            
+            int sharedNumbers = compareArrays(winner,randomArray);
+            
+            switch (sharedNumbers) {
+            case 2:
+                premio+=600;
+                break;
+            case 3:
+                premio+=9500;
+                break;
+            case 4:
+                premio+=100000;
+                break;
+            case 5:
+                premio+=jackpot;
+                break;
+            default:
+                premio+=0;
+        }
             
             // Print the generated array
             for (int num : randomArray) {
@@ -120,17 +166,21 @@ private static void generateRandomArrays(int totalArraysToGenerate ) {
             System.out.println();
         }
     }
+    System.out.print("Numero de veces jugadas:" + totalArraysToGenerate + "\n");
+    System.out.print("Monto gastado en boletos:" + costo + "\n");
+    System.out.print("Monto ganado en boletos:" + premio + "\n");
+    System.out.print("Diferencia total:" + (premio-costo + "\n"));
+    System.out.println("Boleto ganador: " + Arrays.toString(winner));
+    
 }
 
 		
 		
 	public static void main(String[] args) {
 		
-		 int costo = 0;
-		 int premio = 0;
-		 int jackpot = 110300000;
 		 
-		 int totalArraysToGenerate = 10;
+		 
+		 int totalArraysToGenerate = 3;
 		 
 		
 		 
@@ -138,7 +188,7 @@ private static void generateRandomArrays(int totalArraysToGenerate ) {
 	     int r = 5; // Number of values selected in each combination
 	     
 	     
-	     generateRandomArrays(totalArraysToGenerate);
+	     jugarNumeroVeces(totalArraysToGenerate);
 	     
 	    /* List<int[]> combinations = generateCombinations(n, r);
 	     

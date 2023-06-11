efcc54a5d856a1be0b55306053ac056e69826106
Lalord22 <lalo22296@hotmail.com>
2023-06-10 19:10:16 -0600

    Added random array generator

--- src/Lotto.java
+++ src/Lotto.java
@@ -1,7 +1,10 @@
 
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 	
@@ -46,6 +49,79 @@ private static boolean contains(int[] arr, int length, int value) {
     return false;
 }
 
+private static int[] generateRandomArray(int n, int r) {  //generates 1 random array 
+    int[] randomArray = new int[r];
+    Random random = new Random();
+    
+    for (int i = 0; i < r; i++) {
+        randomArray[i] = random.nextInt(n);
+    }
+    
+    return randomArray;
+}
+
+private static int[] generateRandomArray(int n, int r, Set<String> generatedArrays) {
+    int[] randomArray = new int[r];
+    Random random = new Random();
+    
+    for (int i = 0; i < r; i++) {
+        randomArray[i] = random.nextInt(n);
+    }
+    
+    // Convert the array to a string for tracking uniqueness
+    String arrayString = arrayToString(randomArray);
+    
+    // Check if the array has been generated before
+    while (generatedArrays.contains(arrayString)) {
+        for (int i = 0; i < r; i++) {
+            randomArray[i] = random.nextInt(n);
+        }
+        arrayString = arrayToString(randomArray);
+    }
+    
+    return randomArray;
+}
+
+private static String arrayToString(int[] array) {
+    StringBuilder sb = new StringBuilder();
+    for (int num : array) {
+        sb.append(num).append(" ");
+    }
+    return sb.toString();
+}
+
+private static void generateRandomArrays(int totalArraysToGenerate ) {
+	
+	
+	
+	int n = 41; // Total number of values
+    int r = 5; // Number of values to be generated
+     
+    
+    Set<String> generatedArrays = new HashSet<>();
+    
+    for (int i = 0; i < totalArraysToGenerate; i++) {
+        int[] randomArray = generateRandomArray(n, r, generatedArrays);
+        
+        // Convert the array to a string for tracking uniqueness
+        String arrayString = arrayToString(randomArray);
+        
+        // Check if the array has been generated before
+        if (generatedArrays.contains(arrayString)) {
+            System.out.println("Duplicate array generated. Regenerating...");
+            i--; // Decrement i to regenerate a new array in the next iteration
+        } else {
+            generatedArrays.add(arrayString);
+            
+            // Print the generated array
+            for (int num : randomArray) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 		
 		
 	public static void main(String[] args) {
@@ -54,10 +130,17 @@ private static boolean contains(int[] arr, int length, int value) {
 		 int premio = 0;
 		 int jackpot = 110300000;
 		 
+		 int totalArraysToGenerate = 10;
+		 
+		
+		 
 		 int n = 41; // Total number of values
 	     int r = 5; // Number of values selected in each combination
 	     
-	     List<int[]> combinations = generateCombinations(n, r);
+	     
+	     generateRandomArrays(totalArraysToGenerate);
+	     
+	    /* List<int[]> combinations = generateCombinations(n, r);
 	     
 	     for (int[] combination : combinations) {
 	            for (int num : combination) {
@@ -65,7 +148,9 @@ private static boolean contains(int[] arr, int length, int value) {
 	            }
 	            System.out.println();
 	        }
-		
+		*/
+	     
+	     
 
 	}
 
